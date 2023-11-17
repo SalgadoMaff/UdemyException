466285f370ebca403821199066fbcf4ce484cd67
Felipe <lipesmaffini@gmail.com>
2023-11-17 15:53:55 -0400

    Add project files.

---
+++ UdemyException/Entities/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyException.Entities.Exception;
+
+namespace UdemyException.Entities
+{
+    internal class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account() { }
+
+        public Account(int number, string holder)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = 0;
+            WithdrawLimit = 0;
+        }
+        public void deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public void withdraw(double amount)
+        {
+            if (Balance == 0)
+            {
+                throw new DomainException("No funds");
+            }
+            else if (Balance - amount < 0)
+            {
+                throw new DomainException("No enough funds");
+            }
+            else if (amount > WithdrawLimit)
+            {
+                throw new DomainException("The amount exceeds withdraw limit");
+            }
+            else
+            {
+                Balance -= amount;
+            }
+
+        }
+    }
+}
---
+++ UdemyException/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+using System.Linq.Expressions;
+using UdemyException.Entities;
+using UdemyException.Entities.Exception;
+
+int readNumber()
+{
+    Console.Write("Number: ");
+    return int.Parse(Console.ReadLine());
+}
+string readHolder()
+{
+    Console.Write("Holder: ");
+    return Console.ReadLine();
+}
+
+double readInitialBalance()
+{
+    Console.Write("Initial balance: ");
+    return double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+}
+
+double readWithdrawLimit()
+{
+    Console.Write("Withdraw limit: ");
+    return double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+}
+
+    
+
+try
+{
+    Account acc = new Account(readNumber(), readHolder());
+    acc.deposit(readInitialBalance());
+    acc.WithdrawLimit = readWithdrawLimit();
+    Console.Write("\nEnter amount for withdraw: ");
+    double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    acc.withdraw(withdraw);
+    Console.WriteLine("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+}
+catch (DomainException e)
+{
+    Console.WriteLine("Withdraw error: "+e.Message);
+}
+catch(Exception e)
+{
+    Console.WriteLine("Unhandled Exception: " +e.Message);
+}
+
